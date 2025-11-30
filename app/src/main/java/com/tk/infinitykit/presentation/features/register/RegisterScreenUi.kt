dc6c43b4820bd304ffa21b502a277827079cda22
Todor Kosev <todor.kosev@paysafe.com>
2025-11-30 05:32:54 +0200

    Add transition animation to AuthNavHost

--- app/src/main/java/com/tk/infinitykit/presentation/features/register/RegisterScreenUi.kt
+++ app/src/main/java/com/tk/infinitykit/presentation/features/register/RegisterScreenUi.kt
@@ -1,11 +1,16 @@
 package com.tk.infinitykit.presentation.features.register
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -13,14 +18,31 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun RegistrationUi(
+fun RegisterScreenUi(
     viewModel: RegisterViewModel = hiltViewModel(),
     modifier: Modifier = Modifier
 ) {
+    TopAppBar(
+        title = { Text("App") },
+        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.background,
+
+        ),
+        navigationIcon = {
+            IconButton(onClick = {
+
+            }) {
+
+            }
+        }
+    )
     Column(
         modifier = modifier
             .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background)
+
             .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
